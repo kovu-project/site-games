5f2c17a1b0e159a933148ab4c90d12a9ef7c6144
Kovu <kovuwz@gmail.com>
2025-07-10 17:11:38 -0300

    Imagens para a pagina Jogos

--- jogos.php
+++ jogos.php
@@ -7,10 +7,15 @@ require_once 'includes/menu.php';
 <main>
   <h2>Catálogo de Jogos</h2>
   <div class="jogos-lista">
-    <div class="jogo">[Jogo A]</div>
-    <div class="jogo">[Jogo B]</div>
-    <div class="jogo">[Jogo C]</div>
-    <div class="jogo">[Jogo D]</div>
+    <div class="jogo">
+      <img src="imagens/elden-ring.jpg" alt="Elden Ring">
+    </div>
+    <div class="jogo">
+      <img src="imagens/hades.jpg" alt="Hades">
+    </div>
+    <div class="jogo">
+      <img src="imagens/god-of-war.jpg" alt="God of War">
+    </div>
   </div>
 </main>
 
